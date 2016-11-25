5ae89ca978a8cbab5997f39a3dac5b1039ef898f
Carl Zulauf <carl@linkleaf.com>
2016-11-25 11:12:23 -0600

    Simplify redis client initializer

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,8 +1,6 @@
 config = YAML.load_file("config/redis.yml")[Rails.env]
-redis = Redis.new(url: config["url"])
 
-$model_redis =  if config["ns"]
-                  Redis::Namespace.new(config["ns"], redis: redis)
-                else
-                  redis
-                end
+redis = Redis.new(url: config["url"])
+redis = Redis::Namespace.new(config["ns"], redis: redis) if config["ns"]
+
+$model_redis = redis
