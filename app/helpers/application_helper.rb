3d9da848dc441eb04f771cc2c7a3d974bbf8a32e
Carl Zulauf <carl@linkleaf.com>
2018-04-02 23:54:52 -0600

    Scoreboard starting to actually look good

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def init_game_js(game)
     javascript_tag("window.currentGame = new DotsGame(#{game.id.to_json});")
   end
+
+  def nbsp
+    "&nbsp;".html_safe
+  end
 end
