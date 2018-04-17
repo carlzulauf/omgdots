053d54418f55dbe9b5e4f3e81bd0b1071756e1c6
Carl Zulauf <carl@linkleaf.com>
2018-04-17 03:43:35 -0600

    Playing field in a css grid, crudely

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,31 @@ module ApplicationHelper
   def nbsp
     "&nbsp;".html_safe
   end
+
+  def board_css_grid_areas(game)
+    rows = board_css_table(game).map { |row| '"' + row.join(" ") + '"' }
+    areas = rows.join("\n  ");
+    "grid-template-areas: #{areas};"
+  end
+
+  def board_grid_css(game)
+    <<~HERECSS
+      grid-template-columns: repeat(#{game.width}, 10px 50px) 10px;
+      grid-template-rows: repeat(#{game.height}, 10px 50px) 10px;
+    HERECSS
+  end
+
+  def board_css_table(game)
+    ((game.height * 2) + 1).times.map do |row|
+      ((game.width * 2) + 1).times.map do |col|
+        case [row % 2, col % 2]
+        when [0, 0] then "dot"
+        when [0, 1] then "hline"
+        when [1, 0] then "vline"
+        else
+          "tile"
+        end
+      end
+    end
+  end
 end
