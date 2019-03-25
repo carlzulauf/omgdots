3de6926a5ebc36d51652b96f80228dfc7fb3c862
Carl Zulauf <carl@linkleaf.com>
2019-03-25 00:53:48 -0600

    Use BASE_URL env var when available

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,6 +84,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   # config.active_record.dump_schema_after_migration = false
   config.action_cable.allowed_request_origins = [
-    %r{https://omgdots.win}
+    ENV.fetch("BASE_URL", "https://omgdots.vm")
   ]
 end
